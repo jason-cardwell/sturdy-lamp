bf1d79acef2f9448fa47e8451ec0d9459b681120
jason.cardwell <jason.cardwell@b-secur.com>
2024-11-27 15:17:45 +0000

    initial project setup

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello, world!\n\n");
+  
+  printf("This is an experimental project in hosting basic C code on a webserver.\n");
+  printf("It uses emscripten to compile C code to WebAssembly.\n");
+  
+  
+  return 0;
+}
